0e25b98b345c339c853b73e738c902bcd3cbf9ba
AnuragMundada <101085383+AnuragMundada@users.noreply.github.com>
2023-12-08 18:33:11 +0530

    Add files via upload

---
+++ Car_dekho.sql
@@ -0,0 +1,56 @@
+-- Car_dekho Project : Second Hand Car Dealer
+
+create schema cars;
+use cars;
+-- Read Data --
+select * from car_dekho;
+
+-- Total cars: To get a count of total records
+select count(*) from car_dekho;
+-- Result: 7927
+
+-- The manager asked the employee how many cars will be available in 2023?
+select count(*) from car_dekho 
+where year = 2023;
+-- Result: 6
+
+-- The manager asked the employee how many cars is available in 2020,2021,2022
+select year,count(*) from car_dekho
+where year in(2020,2021,2022)
+group by year;
+-- Result: 2022	= 7, 2021 = 7, 2020 = 74
+
+-- Client asked me to print the total of all cars by year. I don't see all the details
+select year,count(*) from car_dekho
+group by year;
+
+-- Client asked to car dealer agent how many diesel cars will there be in 2020? 
+select count(*) from car_dekho
+where year = 2020 and fuel="Diesel";
+-- Result: 20
+
+-- Client requested a car dealer agent How many petrol cars will there be in 2020?
+select count(*) from car_dekho
+where year = 2020 and fuel="Petrol";
+-- Result: 51
+
+-- The manager told the employee to give a print All the fuel cars (petrol, diesel, and CNG) come by all year.
+select year, count(*) from car_dekho where fuel = "Petrol" group by year;
+select year, count(*) from car_dekho where fuel = "Diesel" group by year;
+select year, count(*) from car_dekho where fuel = "CNG" group by year;
+
+-- Manager said there were more than 100 cars in a given year, which year had more than 100 cars?
+select year,count(*) from car_dekho
+group by year 
+having count(*)>100;
+
+-- The manager said to the employee All cars count details between 2015 and 2023 we need a complete list.
+select count(*) from car_dekho
+where year between 2015 and 2023;
+-- Result: 4124
+
+-- The manager said to the employee All cars details between 2015 to 2023 we need complete list
+select * from car_dekho
+where year between 2015 and 2023;
+
+-- END -- 
